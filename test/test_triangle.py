ba8642ab8a47d0eb890d332fd9fc2406cdb60611
luizcdprof <luizcdprof@gmail.com>
2024-08-14 12:33:58 -0300

    testes e app

--- test/test_triangle.py
+++ test/test_triangle.py
@@ -30,3 +30,13 @@ def test_validate_triangle_angles():
     assert not triangle.validate_angles(90, 90, 90)
     assert not triangle.validate_angles(0, 0, 0)
     assert not triangle.validate_angles(-30, 60, 150)
+
+def test_classify_by_sides():
+    assert triangle.classify_by_sides(3, 3, 3) == 'Triângulo Equilátero'
+    assert triangle.classify_by_sides(5, 5, 8) == 'Triângulo Isósceles'
+    assert triangle.classify_by_sides(3, 4, 5) == 'Triângulo Escaleno'
+
+def test_classify_by_angles():
+    assert triangle.classify_by_angles(60, 60, 60) == 'Triângulo Acutângulo'
+    assert triangle.classify_by_angles(90, 45, 45) == 'Triângulo Retângulo'
+    assert triangle.classify_by_angles(120, 30, 30) == 'Triângulo Obtusângulo'
