95160f47355da9d84502c57fda510434ebd9b31a
luizcdprof <luizcdprof@gmail.com>
2024-08-19 09:57:19 -0300

    projeto final

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_from_directory
 import triangle
 
 app = Flask(__name__)
@@ -15,14 +15,20 @@ def index():
         if tipo == 'lado':
             if triangle.validate_sides(valor1, valor2, valor3):
                 classification = triangle.classify_by_sides(valor1, valor2, valor3)
-                return render_template('index.html', classification=classification)
+                triangle.create_triangle_image_by_sides(valor1, valor2, valor3)
+                return render_template('index.html', classification=classification, image='triangle.png')
             else:
                 return render_template('index.html', error='Valores de lado não formam um triângulo válido!')
         else:
             if triangle.validate_angles(valor1, valor2, valor3):
                 classification = triangle.classify_by_angles(valor1, valor2, valor3)
-                return render_template('index.html', classification=classification)
+                triangle.create_triangle_image_by_angles(valor1, valor2, valor3)
+                return render_template('index.html', classification=classification, image='triangle.png')
             else:
                 return render_template('index.html', error='Valores de ângulo não formam um triângulo válido!')
         
     return render_template('index.html')
+
+@app.route('/static/<path:filename>')
+def serve_image(filename):
+    return send_from_directory('static', filename)
--- triangle.py
+++ triangle.py
@@ -1,3 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
 def validate_sides(a, b, c):
     if a <= 0 or b <= 0 or c <= 0:
         return False
@@ -26,3 +30,31 @@ def classify_by_angles(a, b, c):
         return 'Triângulo Acutângulo'
     else:
         return 'Triângulo Obtusângulo'
+
+def create_triangle_image_by_sides(a, b, c):
+    A = np.array([0, 0])
+    B = np.array([a, 0])
+    angle = np.arccos((b**2 + a**2 - c**2) / (2 * b * a))
+    C = np.array([b * np.cos(angle), b * np.sin(angle)])
+    
+    draw_triangle(A, B, C)
+
+def create_triangle_image_by_angles(A, B, C):
+    a = 5
+    b = a * np.sin(np.radians(B)) / np.sin(np.radians(C))
+    c = a * np.sin(np.radians(A)) / np.sin(np.radians(C))
+    
+    create_triangle_image_by_sides(a, b, c)
+
+def draw_triangle(A, B, C):
+    plt.figure()
+    plt.plot([A[0], B[0], C[0], A[0]], [A[1], B[1], C[1], A[1]], 'k-')
+    plt.fill([A[0], B[0], C[0]], [A[1], B[1], C[1]], 'b', alpha=0.3)
+    plt.xlim(-1, max(A[0], B[0], C[0]) + 1)
+    plt.ylim(-1, max(A[1], B[1], C[1]) + 1)
+    plt.gca().set_aspect('equal', adjustable='box')
+    plt.axis('off')
+    
+    img_path = os.path.join('static', 'triangle.png')
+    plt.savefig(img_path)
+    plt.close()
